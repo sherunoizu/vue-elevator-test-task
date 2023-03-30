f7fd08a1bdcf40e484887819a358594fd6701b53
Nikita <nikitazapletin@gmail.com>
2023-03-30 21:15:44 +0300

    feat: store/create store with module structure

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
-import { createStore } from "vuex";
+import { createStore, createLogger } from "vuex";
+import elevatorModule from "./elevatorModule/elevatorModule";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+export const store = createStore({
+  modules: { elevatorModule },
+  
+  plugins: [
+    createLogger(),
+    // TODO: add store sub to set state in localStor
+  ],
 });
