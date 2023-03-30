475b1b85e4a73c000e98190226d09948557f0a72
Nikita <nikitazapletin@gmail.com>
2023-03-30 23:28:05 +0300

    feat: store/implement moving, pick optimal elevator logic

--- src/store/elevatorModule/elevatorModule.js
+++ src/store/elevatorModule/elevatorModule.js
@@ -10,7 +10,13 @@ const {
   RESET_ALL_ELEVATORS,
 } = elevatorTypes;
 
-import { addNewElevator } from "./helpFunctions";
+import { ELEVATOR_STATUS } from "./elevatorConstants";
+
+import {
+  addNewElevator,
+  getLessBusyElevatorId,
+  timeoutElevatorOperationCommits,
+} from "./helpFunctions";
 
 export default {
   state() {
@@ -32,8 +38,68 @@ export default {
 
     overallElevatorStatus: (state) =>
       state.elevatorsData.map((elevator) => elevator.status),
+
+    vacantElevatorIdByCurrentFloor: (state) => (floor) =>
+      state.elevatorsData.findIndex(
+        (elevator) =>
+          elevator.currentFloor === floor &&
+          elevator.status !== ELEVATOR_STATUS.BUSY
+      ),
+
+    elevatorIdExecutingFloor: (state) => (floor) =>
+      state.elevatorsData.findIndex(
+        (elevator) =>
+          elevator.targetFloor === floor &&
+          elevator.status === ELEVATOR_STATUS.BUSY
+      ),
   },
   actions: {
+    processFloor({ commit, state, getters }, floorNumber) {
+      if (getters.elevatorIdExecutingFloor(floorNumber) !== -1) return;
+
+      if (getters.vacantElevatorIdByCurrentFloor(floorNumber) !== -1) return;
+
+      const lessBusyElevatorId = getLessBusyElevatorId(
+        state.elevatorsData,
+        floorNumber
+      );
+
+      commit(ADD_FLOOR_TO_ELEVATOR_QUE, {
+        floorNumber,
+        elevatorId: lessBusyElevatorId,
+      });
+
+      if (
+        state.elevatorsData[lessBusyElevatorId].status ===
+        ELEVATOR_STATUS.VACANT
+      ) {
+        commit(PASS_FLOOR_TO_ELEVATOR, {
+          floorNumber,
+          elevatorId: lessBusyElevatorId,
+        });
+
+        let currentFloor = state.elevatorsData[lessBusyElevatorId].currentFloor;
+        let targetFloor = state.elevatorsData[lessBusyElevatorId].targetFloor;
+
+        timeoutElevatorOperationCommits(commit, {
+          currentFloor,
+          targetFloor,
+          elevatorId: lessBusyElevatorId,
+        });
+      }
+    },
+    continueOperations({ commit }, data) {
+      const elevatorId = data.id;
+      let currentFloor = data.currentFloor;
+      let targetFloor = data.targetFloor;
+
+      timeoutElevatorOperationCommits(commit, {
+        currentFloor,
+        targetFloor,
+        elevatorId,
+      });
+    },
+
     increment({ commit }, settingProp) {
       commit(INCREMENT_SETTING_PROP, settingProp);
     },
@@ -42,13 +108,40 @@ export default {
     },
   },
   mutations: {
-    [ADD_FLOOR_TO_ELEVATOR_QUE]() {},
+    [ADD_FLOOR_TO_ELEVATOR_QUE](state, { floorNumber, elevatorId }) {
+      if (state.elevatorsData[elevatorId].floorsQue.includes(floorNumber)) {
+        return;
+      }
 
-    [PASS_FLOOR_TO_ELEVATOR]() {},
+      state.elevatorsData[elevatorId].floorsQue.push(floorNumber);
+    },
 
-    [CHANGE_ELEVATOR_CURRENT_FLOOR]() {},
+    [PASS_FLOOR_TO_ELEVATOR](state, { floorNumber, elevatorId }) {
+      const floorsQue = { ...state.elevatorsData[elevatorId] }.floorsQue;
 
-    [CHANGE_ELEVATOR_BUSY_STATUS]() {},
+      floorsQue.splice(0, 1);
+
+      state.elevatorsData[elevatorId] = {
+        ...state.elevatorsData[elevatorId],
+        floorsQue,
+        targetFloor: floorNumber,
+        status: ELEVATOR_STATUS.BUSY,
+      };
+    },
+
+    [CHANGE_ELEVATOR_CURRENT_FLOOR](state, { elevatorId, floorNumber }) {
+      state.elevatorsData[elevatorId] = {
+        ...state.elevatorsData[elevatorId],
+        currentFloor: floorNumber,
+      };
+    },
+
+    [CHANGE_ELEVATOR_BUSY_STATUS](state, { elevatorId, status }) {
+      state.elevatorsData[elevatorId] = {
+        ...state.elevatorsData[elevatorId],
+        status: status,
+      };
+    },
 
     [GET_STATE_FROM_STORAGE]() {},
 
--- src/store/elevatorModule/helpFunctions.js
+++ src/store/elevatorModule/helpFunctions.js
@@ -1,4 +1,8 @@
-import { ELEVATOR_STATUS } from "./elevatorConstants";
+import elevatorTypes from "./elevatorTypes";
+const { CHANGE_ELEVATOR_CURRENT_FLOOR, CHANGE_ELEVATOR_BUSY_STATUS } =
+  elevatorTypes;
+
+import { ELEVATOR_STATUS, CHILL_TIME } from "./elevatorConstants";
 
 export const addNewElevator = (newId = 0) => [
   {
@@ -9,3 +13,68 @@ export const addNewElevator = (newId = 0) => [
     floorsQue: [],
   },
 ];
+
+export const getLessBusyElevatorId = (elevatorsData, floorNumber) => {
+  const vacantElevators = elevatorsData.filter(
+    (elevator) => elevator.status === ELEVATOR_STATUS.VACANT
+  );
+
+  if (vacantElevators.length !== 0) {
+    let id = 0;
+    let oldVal = elevatorsData.length + 1;
+
+    vacantElevators.forEach((elevator) => {
+      const newVal = Math.abs(elevator.targetFloor - floorNumber);
+
+      if (newVal <= oldVal) {
+        oldVal = newVal;
+        id = elevator.id;
+      }
+    });
+
+    return id;
+  }
+
+  let id;
+  const targetFloorDistances = elevatorsData.map((elevator) =>
+    elevator.floorsQue.reduce(
+      (prev, curr) => prev + Math.abs(elevator.targetFloor - curr) + 3,
+      0
+    )
+  );
+
+  id = targetFloorDistances.indexOf(Math.min(...targetFloorDistances));
+
+  return id;
+};
+
+export const timeoutElevatorOperationCommits = (
+  commit,
+  { currentFloor, targetFloor, elevatorId }
+) => {
+  let i = currentFloor;
+  const timeout = Math.abs(targetFloor - currentFloor) * 1000;
+
+  commit(CHANGE_ELEVATOR_CURRENT_FLOOR, { elevatorId, floorNumber: i });
+  i < targetFloor ? i++ : i--;
+
+  const interval = setInterval(() => {
+    commit(CHANGE_ELEVATOR_CURRENT_FLOOR, { elevatorId, floorNumber: i });
+    i < targetFloor ? i++ : i--;
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    commit(CHANGE_ELEVATOR_BUSY_STATUS, {
+      elevatorId,
+      status: ELEVATOR_STATUS.CHILL,
+    });
+  }, timeout);
+
+  setTimeout(() => {
+    commit(CHANGE_ELEVATOR_BUSY_STATUS, {
+      elevatorId,
+      status: ELEVATOR_STATUS.VACANT,
+    });
+  }, timeout + CHILL_TIME - 1);
+};
