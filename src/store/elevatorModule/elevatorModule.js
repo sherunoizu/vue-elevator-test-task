be2ec886cb3d7ebc09c25d711d69a44ac5090567
Nikita <nikitazapletin@gmail.com>
2023-03-30 21:23:07 +0300

    feat: store/create elevatorModule

---
+++ src/store/elevatorModule/elevatorModule.js
@@ -0,0 +1,40 @@
+import elevatorTypes from "./elevatorTypes";
+const {
+  ADD_FLOOR_TO_ELEVATOR_QUE,
+  PASS_FLOOR_TO_ELEVATOR,
+  CHANGE_ELEVATOR_CURRENT_FLOOR,
+  CHANGE_ELEVATOR_BUSY_STATUS,
+  GET_STATE_FROM_STORAGE,
+  INCREMENT_SETTING_PROP,
+  DECREMENT_SETTING_PROP,
+  RESET_ALL_ELEVATORS,
+} = elevatorTypes;
+
+export default {
+  state() {
+    return {
+      elevatorsData: [],
+      floorsQuantity: 5,
+      elevatorsQuantity: 1,
+    };
+  },
+  getters: {},
+  actions: {},
+  mutations: {
+    [ADD_FLOOR_TO_ELEVATOR_QUE]() {},
+
+    [PASS_FLOOR_TO_ELEVATOR]() {},
+
+    [CHANGE_ELEVATOR_CURRENT_FLOOR]() {},
+
+    [CHANGE_ELEVATOR_BUSY_STATUS]() {},
+
+    [GET_STATE_FROM_STORAGE]() {},
+
+    [INCREMENT_SETTING_PROP]() {},
+
+    [DECREMENT_SETTING_PROP]() {},
+
+    [RESET_ALL_ELEVATORS]() {},
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import elevatorModule from "./elevatorModule/elevatorModule";
 
 export const store = createStore({
   modules: { elevatorModule },
-  
+
   plugins: [
     createLogger(),
     // TODO: add store sub to set state in localStor
